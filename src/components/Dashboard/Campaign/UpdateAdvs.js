5560295f9abe4249e2e68848f986567c9b102503
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-08-21 14:43:50 +0530

    Update UpdateAdvs.js

--- src/components/Dashboard/Campaign/UpdateAdvs.js
+++ src/components/Dashboard/Campaign/UpdateAdvs.js
@@ -5,6 +5,7 @@ import {useLocation} from "react-router-dom";
 import history from '../../history';
 import { userId } from '../../UserId'
 import { backendHost } from '../../../api-config';
+import { Description } from '@material-ui/icons';
 
 
 export default function UpdatePromo(props){
@@ -13,41 +14,97 @@ export default function UpdatePromo(props){
     const [endDate, setEnd] = useState()
     const [maxLimit, setMax] = useState()
     const [active, setActive] = useState()
+    const [title, setTitle] = useState()
+    const [campaignList,setCampaignList] = useState([])
+    const[adsList,setAdsList] = useState([])
+
+    const [description, setDescription] = useState()
+
+    const [count, setCount] = useState()
+    const[companyList,setCompanyList] = useState([])
+
     const [submitAlert, setAlert] = useState(false)
     const [promoData, setPromo] = useState([])
 
     
     const search = useLocation().search;  
-    const id = new URLSearchParams(search).get('edit');
+    const id = new URLSearchParams(search).get('updatecampaignads');
 
     const fetchPromo = (e) => {
-        axios.get(`${backendHost}/promo/${id}`)
+        axios.get(`${backendHost}/sponsored/get/ads/${id}`)
         .then(res => {
             setPromo(res.data)
-            setCode(res.data[0].promo_code)
-            setStart(res.data[0].promo_start_datetime.split('T')[0])
-            setEnd(res.data[0].promo_end_datetime.split('T')[0])
-            setMax(res.data[0].promo_max_limit)
-            setActive(res.data[0].promo_active)
+            setCode(res.data[0].CampaignName)
+            setStart(res.data[0].StartDate.split('T')[0])
+            setEnd(res.data[0].EndDate.split('T')[0])
+            setMax(res.data[0].DiseaseConditionName)
+            setActive(res.data[0].AdTypeName)
+            setTitle(res.data[0].AdTitle)
+
+            setDescription(res.data[0].AdDescription)
+
+            setCount(res.data[0].AdCount)
+
         })
         .catch(res => {return})
+    }
+
+
+    const getHospital = () => {
+        axios.get(`${backendHost}/article/all/table/disease_condition `)
+        .then(res => {
+            
+            setCompanyList(res.data)
+        })
+        .catch(err => 
+            console.log(err)
+        )
+    }
+    
+    const getAds = () => {
+        axios.get(`${backendHost}/article/all/table/AdsTypes `)
+        .then(res => {
+            
+            setAdsList(res.data)
+        })
+        .catch(err => 
+            console.log(err)
+        )
+    }
+    
+    
+    
+    const getCampaign = () => {
+        axios.get(`${backendHost}/article/all/table/Campaign`)
+        .then(res => {
+            
+            setCampaignList(res.data)
+        })
+        .catch(err => 
+            console.log(err)
+        )
     }
 
     useEffect(() => {
         document.title = "All Cures | Dashboard | Update Promo"
         fetchPromo();
+        getHospital();
+        getAds();
+        getCampaign();
         // eslint-disable-next-line
     }, [])
 
     const submitForm = (e) => {
         e.preventDefault();
-        axios.put(`${backendHost}/promo/${id}`, {
-            "promo_code": code,
-            "promo_start_datetime": startDate,
-            "promo_end_datetime": endDate,
-            "promo_max_limit": maxLimit.toString(),
-            "promo_active": active.toString(),
-            "promo_updated_by": userId
+        axios.put(`${backendHost}/sponsored/update/ad/${id}`,{
+            "CampaignID": code,
+            "StartDate": startDate,
+            "EndDate": endDate,
+            "DiseaseCondition": maxLimit,
+            "AdTypeID": active,
+            "AdTitle": title,
+            "AdDescription": description,
+            "AdCount": count
         })
         .then(res => {
             history.back()
@@ -62,29 +119,77 @@ export default function UpdatePromo(props){
                     <form onSubmit={submitForm}>
                         <div className="row m-4">
                         <Form.Group className="col-md-6 float-left">
-                            <Form.Label>Promo Code</Form.Label>
-                            <Form.Control value={code} onChange={(e) => setCode(e.target.value)} type="text" name=""
-                            placeholder="Promo code here..." required/>
-                        </Form.Group>
+                        <Form.Label>Enter Campaign Name</Form.Label>
+                        <Form.Control
+                         as="select"
+                            value={code}
+                            onChange={(e) => setCode(e.target.value)}
+                            type="text"
+                            placeholder="Campaign here..."
+                            required
+                        >
+                        <option>{code}</option> {/* Display code from fetchPromo */}
+                        <option>Select Campaign</option>
+                            {campaignList.map((c) => (
+                            <option value={c[0]}>{c[2]}</option>
+                            ))}
+                                                    </Form.Control>
+
+                    </Form.Group>
                         <Form.Group className="col-md-6 float-left">
-                            <Form.Label>Campaign Start Date</Form.Label>
+                            <Form.Label>AD Start Date</Form.Label>
                             <Form.Control type="Date" defaultValue={startDate} onChange={(e) => setStart(e.target.value)} name=""
                             placeholder="Start Date here..." required/>
                         </Form.Group>
                         <Form.Group className="col-md-6 float-left">
-                            <Form.Label>Campaign End Date</Form.Label>
+                            <Form.Label>AD End Date</Form.Label>
                             <Form.Control defaultValue={endDate} onChange={(e) => setEnd(e.target.value)} type="Date" name=""
                             placeholder="Start Date here..." required/>
                         </Form.Group>
                         <Form.Group className="col-md-6 float-left">
-                            <Form.Label>Campaign's Maximum Limit</Form.Label>
-                            <Form.Control type="text" name="" value={maxLimit} onChange={(e) => setMax(e.target.value)} 
-                            placeholder="Maximum Limit here..." required/>
+                        <Form.Label>Enter DiseaseCondition</Form.Label>
+                        <Form.Control
+                            as="select"
+                            value={maxLimit}
+                            onChange={(e) => setMax(e.target.value)}
+                            required
+                        >
+                            <option value={maxLimit}>{maxLimit}</option> {/* Display maxLimit from fetchPromo */}
+                            <option>Select Disease Condition</option>
+                            {companyList.map((c) => (
+                            <option value={c[0]}>{c[1]}</option>
+                            ))}
+                        </Form.Control>
+                    </Form.Group>
+                        <Form.Group className="col-md-6 float-left">
+                            <Form.Label>Enter Ad Type</Form.Label>
+                            <Form.Control   as="select" type="text" name="" value={active} onChange={e => setActive(e.target.value)}
+
+                             required>
+                                   <option value={active}>{active}</option> {/* Display maxLimit from fetchPromo */}
+                            <option>Select Ad Type</option>
+                            {adsList.map((c) => {
+        
+        return (
+            <option value={c[0]}>{c[1]}</option>
+        )
+    })}
+                        </Form.Control>
                         </Form.Group>
                         <Form.Group className="col-md-6 float-left">
-                            <Form.Label>Campaign's Active Status</Form.Label>
-                            <Form.Control type="text" name="" value={active} onChange={e => setActive(e.target.value)}
-                             placeholder="1 for active & 0 for not-active..." required/>
+                            <Form.Label>Enter Ad Title</Form.Label>
+                            <Form.Control type="text" name="" value={title} onChange={e => setTitle(e.target.value)}
+                            required/>
+                        </Form.Group>
+                        <Form.Group className="col-md-6 float-left">
+                            <Form.Label>Enter Ad Description</Form.Label>
+                            <Form.Control type="text" name="" value={description} onChange={e => setDescription(e.target.value)}
+                             required/>
+                        </Form.Group>
+                        <Form.Group className="col-md-6 float-left">
+                            <Form.Label>Enter Ad Impression</Form.Label>
+                            <Form.Control type="text" name="" value={count} onChange={e => setCount(e.target.value)}
+                             required/>
                         </Form.Group>
                         </div>
                         {
