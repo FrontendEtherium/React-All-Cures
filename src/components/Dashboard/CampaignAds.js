c37499a449b283213ff14dc61022c889f361de13
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-08-21 14:50:29 +0530

    Update CampaignAds.js

--- src/components/Dashboard/CampaignAds.js
+++ src/components/Dashboard/CampaignAds.js
@@ -16,45 +16,114 @@ function App() {
   const [middle, setMiddle] = useState();
   const [last, setLast] = useState();
   const [status,setStatus] = useState('')
-  const [authorAddr, setAddr] = useState()
+  const [campaignList,setCampaignList] = useState([])
   const [emaill, setEmail] = useState()
   const [type,setType] = useState()
-  const[id,setId] = useState()
+  const[alt,setAlt] = useState()
  const[pinCode,setPinCode] = useState()
  const [startDate, setStart] = useState(Date)
  const [endDate, setEnd] = useState(Date)
   const [alert,setAlert] = useState()
   const [image, setImage] = useState(null);
+  const[companyList,setCompanyList] = useState([])
+  const[adsList,setAdsList] = useState([])
+
 
   
-  const submitForm = (e) => {
+  const submitForm = async (e) => {
     e.preventDefault();
-    axios.post(`${backendHost}/sponsored/create/ad`, {
-        "CampaignID": first,
-        "DiseaseCondition": middle,
-        "AdTypeID": last,
-        "AdTitle": type,
-        "AdDescription": pinCode,
-        "AdCount": emaill,
-        "StartDate": startDate,
-        "EndDate": endDate,
-        
+  
+    const AdMap = {
+      CampaignID: first,
+      DiseaseCondition: middle,
+      AdTypeID: last,
+      AdTitle: type,
+      AdDescription: pinCode,
+      AdCount: emaill,
+      StartDate: startDate,
+      EndDate: endDate,
+      ImageAltText: alt,
+    };
+  
+    // Log AdMap data before sending
+    console.log('AdMap:', AdMap);
+  
+    const formData = new FormData();
+    formData.append('AdMap', JSON.stringify(AdMap)); // Convert AdMap object to JSON and append it to formData
+    if (image) {
+      formData.append('image', image);
+    }
+  
+    axios.post(`${backendHost}/sponsored/create/ad`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
     })
-    .then(res => {
-        setAlert(true)
-        setTimeout(() => {
-            setAlert(false)
-        }, 4000);
+    .then(() => {
+      setAlert(true);
+      setTimeout(() => {
+        setAlert(false);
+      }, 4000);
     })
-    .catch(res => console.log(res))
-}
+    .catch((error) => {
+      console.error('Error creating ad:', error);
+    });
+  };
+  
 
 const handleImageChange = (e) => {
     const file = e.target.files[0];
     setImage(file);
-  };
 
+    console.log("Image selected:", {
+      image: file.name,
+    });
+  };
  
+ 
+
+const getHospital = () => {
+    axios.get(`${backendHost}/article/all/table/disease_condition `)
+    .then(res => {
+        
+        setCompanyList(res.data)
+    })
+    .catch(err => 
+        console.log(err)
+    )
+}
+
+const getAds = () => {
+    axios.get(`${backendHost}/article/all/table/AdsTypes `)
+    .then(res => {
+        
+        setAdsList(res.data)
+    })
+    .catch(err => 
+        console.log(err)
+    )
+}
+
+
+
+const getCampaign = () => {
+    axios.get(`${backendHost}/article/all/table/Campaign`)
+    .then(res => {
+        
+        setCampaignList(res.data)
+    })
+    .catch(err => 
+        console.log(err)
+    )
+}
+
+useEffect(() => {
+  
+    getHospital()
+    getAds()
+    getCampaign()
+
+}, []) 
 
   return (
       
@@ -63,28 +132,55 @@ const handleImageChange = (e) => {
           <div className="promo-page">
                 <div className="container">
                 <div className="card my-3">
-                        <div className="card-title h3 text-center py-2 border-bottom"> <b> (Create Campaign Ads)</b></div>
+                        <div className="card-title h3 text-center py-2 border-bottom"> <b> (Create AD)</b></div>
                         <form onSubmit={submitForm}>
                             <div className="row m-4">
-                        <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
-                            <Form.Label>Enter Campaign Name  <b> (Required)</b></Form.Label>
-                            <Form.Control value={first} onChange={(e) => setFirst(e.target.value)}  type="text" name=""
-                            placeholder="Enter Campaign Name..." required/>
-                        </Form.Group>
-                        <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
-                            <Form.Label>Enter DiseaseCondition  <b> (Optional)</b></Form.Label>
-                            <Form.Control  value={middle} onChange={(e) => setMiddle(e.target.value)} type="text" name=""
-                            placeholder="Enter DiseaseCondition..." />
+                       
+                       
+         
+                       <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
+                        <label htmlFor="">Enter Campaign <b>(Required)</b></label>
+                        <select name="hospital" value={first} onChange={(e) => setFirst(e.target.value)}  placeholder=" Enter Company" required="" className="form-control">
+<option>Select Campaign</option>
+    {campaignList.map((c) => {
+        
+        return (
+            <option value={c[0]}>{c[2]}</option>
+        )
+    })}
+</select>
                         </Form.Group>
 
                         <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
-                            <Form.Label>Enter  AdTypeName  <b> (Optional)</b></Form.Label>
-                            <Form.Control  value={last} onChange={(e) => setLast(e.target.value)} type="text" name=""
-                            placeholder="Enter  AdTypeName..." />
+                        <label htmlFor="">Enter DiseaseCondition  <b>(Required)</b></label>
+                        <select name="hospital" value={middle} onChange={(e) => setMiddle(e.target.value)} placeholder=" Enter Company" required="" className="form-control">
+<option>Select Diseases</option>
+    {companyList.map((c) => {
+        
+        return (
+            <option value={c[0]}>{c[1]}</option>
+        )
+    })}
+</select>
+                        </Form.Group>
+
+                    
+
+                        <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
+                        <label htmlFor="">Enter Ad Type  <b>(Required)</b></label>
+                        <select name="hospital" value={last} onChange={(e) => setLast(e.target.value)} placeholder=" Enter Company" required="" className="form-control">
+  <option>Select Ads Type</option>
+    {adsList.map((c) => {
+        
+        return (
+            <option value={c[0]}>{c[1]}</option>
+        )
+    })}
+</select>
                         </Form.Group>
 
                         <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
-                            <Form.Label>Enter Ad Title  <b> (Optional)</b></Form.Label>
+                            <Form.Label>Enter AD Title  <b> (Optional)</b></Form.Label>
                             <Form.Control  value={type} onChange={(e) => setType(e.target.value)} type="text" name=""
                             placeholder="Enter Ad Title..." />
                         </Form.Group>
@@ -96,14 +192,14 @@ const handleImageChange = (e) => {
                         </Form.Group>
 
                         <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
-                            <Form.Label>Enter Ad Impression <b> (Optional)</b></Form.Label>
+                            <Form.Label>Enter AD Impressions  <b>(Required)</b></Form.Label>
                             <Form.Control  value={emaill} onChange={(e) => setEmail(e.target.value)} type="text" name=""
                             placeholder="Enter AdCount..." />
                         </Form.Group>
                         
                         <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
                             <Form.Label>Enter ImageAltText  <b> (Optional)</b></Form.Label>
-                            <Form.Control  value={middle} onChange={(e) => setMiddle(e.target.value)} type="text" name=""
+                            <Form.Control  value={alt} onChange={(e) => setAlt(e.target.value)} type="text" name=""
                             placeholder="Enter ImageAltText..." />
                         </Form.Group>
                         <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
@@ -117,13 +213,13 @@ const handleImageChange = (e) => {
     </Form.Group>
 
     <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
-                            <Form.Label>Campaign Start Date</Form.Label>
+                            <Form.Label>AD Start Date</Form.Label>
                             <Form.Control type="Date" value={startDate} onChange={(e) => setStart(e.target.value)} name=""
                             placeholder="Start Date here..." required/>
                         </Form.Group>
                         
                         <Form.Group className="col-md-6 float-left" style={{zIndex: 2}}>
-                            <Form.Label>Campaign End Date</Form.Label>
+                            <Form.Label>AD End Date</Form.Label>
                             <Form.Control value={endDate} onChange={(e) => setEnd(e.target.value)} type="Date" name=""
                             placeholder="Start Date here..." required/>
                         </Form.Group>
