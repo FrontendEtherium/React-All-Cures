5abb5291cb7d2bac20e0f25b901265cebb59d3c2
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-08-21 14:38:08 +0530

    Update CampaignAdvs.js

--- src/components/Dashboard/Campaign/CampaignAdvs.js
+++ src/components/Dashboard/Campaign/CampaignAdvs.js
@@ -22,59 +22,81 @@ const AllAdds = () => {
         fetchad();
     }, [])
 
-    const PromoDelete = (promoId) => {
-        axios.delete(`${backendHost}/promo/${promoId}`)
-        .then(res => {
-            fetchad()
-        })
-        .catch(err => {
-            return;
-        })
-    }
+  
   return (
     <>
       <div className="container mb-4">
                 <div className="row">
             {
                 allAdds?
-                allAdds.map(i => {
+                allAdds.filter(item=>item.ReviewStatus===1)
+                .map(i => {
                     return(
                         <div className="card col-md-5 mt-5 mx-3 border p-3 h6">
-                            <div className="card-title h4"><span className="font-weight-bold">Ad Title: </span>{i.AdTitle}</div>
+                            <div className="card-title h4"><span className="font-weight-bold">CampaignName: </span>{i.CampaignName}</div>
                             {/* <div className="card-body"> */}
-                               
+                            <div className="pb-2"><span className="font-weight-bold">DiseaseConditionName:</span> {i.DiseaseConditionName}</div>
+                            <div className="pb-2"><span className="font-weight-bold">AdTypeName:</span> {i.AdTypeName}</div>
+
                                 <div className="pb-2"><span className="font-weight-bold">Ad Description:</span> {i.AdDescription}</div>
                                 <div className="pb-2"><span className="font-weight-bold">Ad Count:</span> {i.AdCount}</div>
-                               <div className="pb-2"><span className="font-weight-bold">Ad Delivered:</span> {i.AdDelivered}</div>
-                                <div className="pb-2"><span className="font-weight-bold"></span><img src= {i.ImageLocation}/></div>
+                               <div className="pb-2"><span className="font-weight-bold">AdTitle:</span> {i.AdTitle}</div>
                                 <div className="pb-2"><span className="font-weight-bold">Start Date:</span> {i.StartDate}</div>
                                 <div className="pb-2"><span className="font-weight-bold">End Date:</span> {i.EndDate}</div>
-                                <div className="pb-2"><span className="font-weight-bold">Created On::</span> {i.CreateDate}</div>
-                                <div className="pb-2"><span className="font-weight-bold">Last Updated:</span> {i.LastUpdatedDate}</div>
-                                <div className="pb-2"><span className="font-weight-bold">Review Status:</span> {i.ReviewStatus}</div>
-                                <div className="pb-2"><span className="font-weight-bold">Payment Status:</span> {i.PaymentStatus}</div>
+                        
                                
-                                <div className="row mx-1 my-2">
-                            <Link to={`/dashboard?edit=${i.promo_id}`} className="col-md-3 btn mr-2" style={{backgroundColor: '#9289be', color: '#fff'}}>Edit</Link>
-                            {
-                                i.promo_active === 1?
-                                    <button onClick={() => {
-                                        const confirmBox = window.confirm(
-                                            "Are you sure?"
-                                        )
-                                        if (confirmBox === true) {
-                                            PromoDelete(i.promo_id)
-                                        }
-                                    }} className="col-md-4 btn btn-dark">De-activate</button>
-                                    : <button className="col-md-4 btn btn-dark" disabled>De-activate</button>
-                            }
+
+                               <div className="row mx-1 my-2">
                             
+                            <Link to={`/dashboard?updatecampaignads=${i.AdID}`} className="col-md-3 btn mr-2" style={{backgroundColor: '#9289be', color: '#fff'}}>Edit</Link>
+                          
+                                <button className="col-md-4 btn btn-dark ml-2">Active</button>
+
                             </div>
+
+
                         </div>
                     )
                 })
                 : null
             }
+
+
+{
+                allAdds?
+                allAdds.filter(item=>item.ReviewStatus===0)
+                .map(i => {
+                    return(
+                        <div className="card col-md-5 mt-5 mx-3 border p-3 h6">
+                            <div className="card-title h4"><span className="font-weight-bold">CampaignName: </span>{i.CampaignName}</div>
+                            {/* <div className="card-body"> */}
+                            <div className="pb-2"><span className="font-weight-bold">DiseaseConditionName:</span> {i.DiseaseConditionName}</div>
+                            <div className="pb-2"><span className="font-weight-bold">AdTypeName:</span> {i.AdTypeName}</div>
+
+                                <div className="pb-2"><span className="font-weight-bold">Ad Description:</span> {i.AdDescription}</div>
+                                <div className="pb-2"><span className="font-weight-bold">Ad Count:</span> {i.AdCount}</div>
+                               <div className="pb-2"><span className="font-weight-bold">AdTitle:</span> {i.AdTitle}</div>
+                                <div className="pb-2"><span className="font-weight-bold">Start Date:</span> {i.StartDate}</div>
+                                <div className="pb-2"><span className="font-weight-bold">End Date:</span> {i.EndDate}</div>
+                        
+                               
+
+                               <div className="row mx-1 my-2">
+                            
+                            <Link to={`/dashboard?updatecampaignads=${i.AdID}`} className="col-md-3 btn mr-2" style={{backgroundColor: '#9289be', color: '#fff'}}>Edit</Link>
+                          
+                                <button className="col-md-4 btn btn-dark ml-2" disabled>Not-active</button>
+
+                            </div>
+
+
+                        </div>
+                    )
+                })
+                : null
+            }
+
+
             </div>
             </div>
     </>
