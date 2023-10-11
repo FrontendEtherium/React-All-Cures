e9af5b15c0dd8611d430c6ab7a7f6ba06bfcdbf8
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-10-11 13:22:07 +0530

    Update CarouselPreview.js

--- src/components/Disease/CarouselPreview.js
+++ src/components/Disease/CarouselPreview.js
@@ -50,8 +50,21 @@ const CarouselPreview = (props) => {
     //   }
 
     function allPosts() {                        // For all available blogs "/blogs"
-        fetch(`${backendHost}/article/allkv`)
-          .then((res) => res.json())
+
+        const headers = new Headers({
+            'Authorization': 'Bearer local@7KpRq3XvF9' 
+          });
+
+        fetch(`${backendHost}/article/allkv`,{
+            method: 'GET',
+            headers: headers
+        })
+        .then((res) => {
+            if (!res.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return res.json();
+          })
           .then((json) => {
             var temp = []
             if(articleFilter === props.dcName){
