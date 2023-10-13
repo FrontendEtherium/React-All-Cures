d3267774e8244fd804b6385b4313cf589e7326f5
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-10-13 16:13:14 +0530

    Update AppBanner.js

--- src/components/LandingPage/AppBanner.js
+++ src/components/LandingPage/AppBanner.js
@@ -4,7 +4,7 @@ import './AppBanner.css'
 
 
 const AppBanner = () => {
-  const [showPopup, setShowPopup] = useState(false);
+ const [showPopup, setShowPopup] = useState(false);
   const [isAppInstalled, setIsAppInstalled] = useState(false);
 
   useEffect(() => {
@@ -13,26 +13,18 @@ const AppBanner = () => {
 
     if (isMobileDevice) {
           
-     setTimeout(() => {    
       setShowPopup(true);
-    }, 2000); 
     }
   }, []);
 
-   const openAppOrStore = () => {
+  const openAppOrStore = () => {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
         
         if (/android/i.test(userAgent)) {
-          
-        
-                // Redirect to Play Store if the app didn't open in a reasonable time
-                window.location.href = 'https://play.google.com/store/apps/details?id=com.allcures&hl=en&gl=US';
-             
-        } if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-          
+            window.location.href = 'https://www.all-cures.com/appRedirect'; 
             
-                // Redirect to App Store if the app didn't open in a reasonable time
-                window.location.href = 'https://apps.apple.com/in/app/all-cures/id1659590351';
+        } if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            window.location.href = 'https://www.all-cures.com/appRedirect'; 
             
         } 
 
@@ -41,16 +33,17 @@ const AppBanner = () => {
 
       }
 
-  
+
+
 
 
   return (
     <div>
       {showPopup && (
-        <div id="app-bnr"className="">
-         <div className=" mb-2 d-flex justify-content-center">  <p style={{textAlign:"center"}}>Download our app for the best experience.</p></div>
-         <div className=" d-flex justify-content-center" > <button  onClick={openAppOrStore} className=" btn btn-secondary mb-2">Open in App</button></div>
-         <div className=" d-flex justify-content-center"> <button onClick={() => setShowPopup(false)}  className=" btn btn-secondary mb-2">Stay on the Website</button></div>
+        <div id="app-bnr">
+          <p> Download our app for the best experience.</p>
+          <button  onClick={openAppOrStore} className=" btn btn-secondary mb-2 mr-3">Open in App</button>
+          <button onClick={() => setShowPopup(false)}  className=" btn btn-secondary mb-2">Continue on the Website</button>
         </div>
       )}
 
