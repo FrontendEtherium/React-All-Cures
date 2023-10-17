90baba552572a5ec95f50a0d47b1d324d5c652fb
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-10-17 14:06:16 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -194,7 +194,7 @@ import { userAccess } from "../UserAccess";
                      <div className="row">
                         <div className="header" style={{width:"100%"}}>
                            <div className=" logo mt-3"> 
-                              <Link to='/home'>
+                              <Link to='/'>
                                 <img src={Heart} alt="All Cures Logo"/>
                                 <span>All Cures</span>
                               </Link>
@@ -212,7 +212,7 @@ import { userAccess } from "../UserAccess";
                                <div class="collapse navbar-collapse" id="navbarNavDropdown">
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="/home">Home</a>
+      <a class="nav-link" href="/">Home</a>
     </li>
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</a>
