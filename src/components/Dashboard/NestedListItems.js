8206ce2ae8c745cbbf7e3f81d49902c18b67cd88
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-10-19 11:22:18 +0530

    Update NestedListItems.js

--- src/components/Dashboard/NestedListItems.js
+++ src/components/Dashboard/NestedListItems.js
@@ -395,7 +395,16 @@ setOpenn(!openn);
 
         </List>
       </Collapse>
-      
+
+               <ListItem button style={{backgroundColor:'lightblue'}}>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <Link to="/dashboard?createwebstories">
+          <ListItemText primary="Create Webstories" />
+        </Link>
+      </ListItem>
+
     </List>
   );
 }
