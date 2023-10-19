9eebe89329767c30a3ee400bde8057b4ee8e284c
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-10-19 12:53:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Main from './components/MainComponent';
 import CookieConsent from "react-cookie-consent";
+import AppBanner from "./components/LandingPage/AppBanner";
+
 
 function App() {
   
@@ -12,6 +14,7 @@ function App() {
       We use cookies to ensure you have the best browsing experience on our website. By using our site, you acknowledge that you have read and understood our <a className="text-underline text-white" href="/">Cookie Policy</a> & <a className="text-underline text-white" href="https://etheriumtech.com/privacy.html" target="_blank" rel="noreferrer">Privacy Policy</a>.
       </div>
       </CookieConsent>
+     <AppBanner/>
     </div>
   );
 }
