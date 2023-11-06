2c6351525b8eb09344de5a68239a8e3f4f1bba5f
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-11-06 11:44:04 +0530

    Update EditModal.js

--- src/components/BlogPage/EditModal.js
+++ src/components/BlogPage/EditModal.js
@@ -1022,7 +1022,7 @@ By visiting this page on our website: <a href="https://www.all-cures.com">www.al
                         <div className='h5'>Content JSON Preview</div>
                         <textarea 
                             className='w-100' 
-                            value={contentSmall} 
+                            value={JSON.stringify(contentSmall)} 
                             onChange={(e)=> {
                                 setContentSmall(e.target.value)
                                 IsJsonValid(e.target.value)
