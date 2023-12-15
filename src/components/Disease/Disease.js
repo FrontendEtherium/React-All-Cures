9399df79d26bc8692104946827165f2f61f6383d
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-12-15 13:12:57 +0530

    Update Disease.js

--- src/components/Disease/Disease.js
+++ src/components/Disease/Disease.js
@@ -854,7 +854,7 @@ componentDidMount() {
         </HelmetMetaData>
         <div className="ad-spac">
         <button className="btn" data-toggle="modal"data-target=".bd-example-modal-lg">
-          <img src={AyurvedaAd} alt="advertisment"/>
+          <img src={'https://ik.imagekit.io/qi0xxmh2w/productimages/tr:w-900,f-webp/static/media/97x90%20Plain.395a48b7.jpg'} alt="advertisment"/>
      
             </button>
         </div>
@@ -891,7 +891,7 @@ componentDidMount() {
                                />
                                </div>:
                               <button className="btn pl-4 mt-2 " id="left-menu-ad" data-toggle="modal"data-target=".bd-example-modal-lg">
-                                 <img className="pl-4" src={PersianAd} alt="adhhh"
+                                 <img className="pl-4" src={'https://ik.imagekit.io/qi0xxmh2w/productimages/tr:w-180,f-webp/static/media/Persian.954aca12.jpg'} alt="adhhh"
                                  />
                                  </button>
                                  :null
