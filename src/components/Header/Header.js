c7dbe4bb3c2ce3f2003cd6d2ff544ed885e28e0e
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-12-15 16:16:57 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -195,7 +195,7 @@ import { userAccess } from "../UserAccess";
                         <div className="header" style={{width:"100%"}}>
                            <div className=" logo mt-3"> 
                               <Link to='/'>
-                                <img src={Heart} alt="All Cures Logo"/>
+                                <img src={'https://ik.imagekit.io/qi0xxmh2w/productimages/tr:w-300,f-webp/static/media/heart.30b5f0b3cbbf79450c5a.png'} alt="All Cures Logo"/>
                                 <span>All Cures</span>
                               </Link>
                            </div>
