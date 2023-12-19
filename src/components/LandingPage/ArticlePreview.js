ed32d6d7eddacfacd8dd1b7ea58ea6f863e20c94
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2023-12-19 12:51:26 +0530

    Update ArticlePreview.js

--- src/components/LandingPage/ArticlePreview.js
+++ src/components/LandingPage/ArticlePreview.js
@@ -345,12 +345,11 @@ const ArticlePreview = (props) => {
                     if(i.content){
                         content = IsJsonValid(decodeURIComponent(i.content))
                     }
-                    if(imgLocation && imgLocation.includes('cures_articleimages')){
-                        imageLoc = `https://all-cures.com:444/`+imgLocation.replace('json', 'png').split('/webapps/')[1]
+                     if(imgLocation && imgLocation.includes('cures_articleimages')){
+                        imageLoc = `https://ik.imagekit.io/hg4fpytvry/product-images/tr:w-300,f-webp/`+imgLocation.replace('json', 'png').split('/webapps/')[1]
                     } else {
-                        imageLoc = 'https://all-cures.com:444/cures_articleimages//299/default.png'
+                        imageLoc = ' https://ik.imagekit.io/hg4fpytvry/product-images/tr:w-300,f-webp/cures_articleimages//299/default.png'
                     }
-
                     var title = i.title
                     var regex = new RegExp(' ', 'g');
 
