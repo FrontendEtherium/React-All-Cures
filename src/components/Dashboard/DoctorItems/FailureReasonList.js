2cd4de14270f9f2998e8bca8b51515b7c5d69af7
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-01-12 15:16:08 +0530

    Update FailureReasonList.js

--- src/components/Dashboard/DoctorItems/FailureReasonList.js
+++ src/components/Dashboard/DoctorItems/FailureReasonList.js
@@ -30,7 +30,7 @@ const  FailureReasonList = () => {
     }, [])
     const AvailDelete = (ServiceId) => {
         console.log('delete')
-        axios.delete(`${backendHost}/payment/delete/${ServiceId}`)
+        axios.delete(`${backendHost}/video/delete/failure/reason/${ServiceId}`)
         .then(res => {
            
         })
@@ -54,28 +54,36 @@ const  FailureReasonList = () => {
                         <div className="card col-md-5 mt-5 mx-3 border p-3 h6">
                             <div className="card-title h4"><span className="font-weight-bold">Failure Id: </span>{i.failureID}</div>
                             <div className="pb-2"><span className="font-weight-bold">Reason:</span> {i.reasons}</div>
-                                <div className="pb-2"><span className="font-weight-bold">Refund:</span> {i.refund}</div>
-                                <div className="pb-2"><span className="font-weight-bold">Penalty:</span> {i.penalty}</div>
+                                <div className="pb-2"><span className="font-weight-bold">Refund:</span>Rs {i.refund}</div>
+                                <div className="pb-2"><span className="font-weight-bold">Penalty:</span>Rs {i.penalty}</div>
                                <div className="pb-2"><span className="font-weight-bold">Created by:</span> {i.createdBy}</div>
                                
-                               <div className="pb-2"><span className="font-weight-bold">Created on:</span> {i.createdDate}</div>
+                               <div className="pb-2"><span className="font-weight-bold">Created on:</span> {i.createdDate.split('T')[0]}</div>
                                {/* <div className="pb-2"><span className="font-weight-bold">Created on:</span> {i.slotDuration(min)}</div> */}
-                               <div className="pb-2"><span className="font-weight-bold">Last Updated  Date:</span> {i.lastUpdatedDate}</div>
+                               <div className="pb-2"><span className="font-weight-bold">Last Updated  Date:</span> {i.lastUpdatedDate.split('T')[0]}</div>
                                <div className="pb-2"><span className="font-weight-bold">Updated By:</span> {i.updatedBy}</div>
-                               <div className="pb-2"><span className="font-weight-bold">Status:</span> {i.status}</div>
+                               <div className="pb-2"><span className="font-weight-bold">Status:</span> {i.status ===1?
+                                <span> Active</span>
+                                : <span> Inactive</span>}</div>
                               
                               
                             <div className="row mx-1 my-2">
                             <Link to={`/dashboard?updatefailurereasonlist=${i.failureID}`} className="col-md-3 btn mr-2" style={{backgroundColor: '#9289be', color: '#fff'}}>Edit</Link>
                            
+
+
+                            { i.status===1?
                                       <button onClick={() => {
                                         const confirmBox = window.confirm(
                                             "Are you sure?"
                                         )
                                         if (confirmBox === true) {
-                                            AvailDelete(i.servicePaymentMasterID)
+                                            AvailDelete(i.failureID)
                                         }
-                                    }} className="col-md-4 btn btn-dark">Delete</button>
+                                    }} className="col-md-4 btn btn-dark">De-activate</button>
+                                    :<button className="col-md-4 btn btn-dark" disabled>De-activated</button>
+
+                                }
 
                             
                             </div>
