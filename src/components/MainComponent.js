c4fb7037ff22991b9df9cc7e207f9d7f01c84158
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-02-08 17:04:49 +0530

    Update MainComponent.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -225,7 +225,7 @@ const Routes = (props) => {
       <Route auth={Auth.auth} exact path="/cure/:id" component={Disease}/>
       
       {/* Doctor profile page */}
-      <Route auth={Auth.auth} exact path="/profile/:id" component={Profile} />
+       <Route auth={Auth.auth} exact path="/doctor/:id" component={Profile} />
       <Route exact path="/profile/:id/edit" component={LoginInfo} />
    <Route exact path="/privacy" component={privacy}/>
       {/* Doctor invitation page and ask for UPNR number */}
