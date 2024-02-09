36668098dff9a0edc0ab34bd34ec76d677aa377e
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-02-09 11:45:32 +0530

    Update Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -181,7 +181,7 @@ class Search extends Component {
                       </div>
                         {items.map((i) => ( 
                           <ProfileTab
-                            rowno = {i.map.rowno}
+                            rowno = {i.map.docID}
                             docid= {i.map.doctorid}
                             firstName = {i.map.docname_first}
                             lastName = {i.map.docname_last}
@@ -197,6 +197,7 @@ class Search extends Component {
                             key = {i.map.doctorid}
                             eduTraining={i.map.edu_training}
                             ratingVal={this.state.ratingvalue}
+                            img={i.map.img_Loc}
                           />
                         ))}
                         <Test
