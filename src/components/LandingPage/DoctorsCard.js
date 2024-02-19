5162d46cfab5bbd9715eadb05390e9814b66e453
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-02-19 16:50:34 +0530

    Update DoctorsCard.js

--- src/components/LandingPage/DoctorsCard.js
+++ src/components/LandingPage/DoctorsCard.js
@@ -4,49 +4,58 @@ import { Link } from 'react-router-dom';
 import { backendHost } from '../../api-config';
 import DummyDoc from "../../assets/healthcare/img/images/defaultDoc1.png";
 
-const DoctorsCard = ({rowno, firstName, lastName, primary_spl, hospital_affliated, state, country_code}) => {
+const DoctorsCard = ({docID, firstName, lastName, primarySpl, hospitalAffliated, state, country,img}) => {
    
 const[doctImage,setDoctImage]=useState([])
 const [isDefaultImage, setIsDefaultImage] = useState(false);
    
-   useEffect(() => {
-   fetch(`${backendHost}/data/doctor/image`)
-       .then((res) => res.json())
-       .then((json) => {
 
-   let matchedImageLoc = DummyDoc; 
 
-   for (let i = 0; i < json.length; i++) {
-       if (json[i].rowno == rowno && json[i].img_Loc !=null) {
+console.log('docimg',img)
+//    useEffect(() => {
+//    fetch(`${backendHost}/data/doctor/image`)
+//        .then((res) => res.json())
+//        .then((json) => {
+
+//    let matchedImageLoc = DummyDoc; 
+
+//    for (let i = 0; i < json.length; i++) {
+//        if (json[i].docID == docID && json[i].img_Loc !=null) {
 
         
-           matchedImageLoc = `https://ik.imagekit.io/hg4fpytvry/product-images/tr:w-180,h-230,f-webp${json[i].img_Loc}`;
+//            matchedImageLoc = `https://ik.imagekit.io/hg4fpytvry/product-images/tr:w-300,f-webp${json[i].img_Loc}`;
            
-           break; // Break the loop once a match is found
-       }
+//            break; // Break the loop once a match is found
+//        }
 
-       else if(json[i].rowno == rowno && json[i].img_Loc ==null){
+//        else if(json[i].docID == docID && json[i].img_Loc ==null){
 
-         matchedImageLoc = DummyDoc;
-         setIsDefaultImage(true)
-        }
-   }
+//          matchedImageLoc = DummyDoc;
+//          setIsDefaultImage(true)
+//         }
+//    }
 
-   setDoctImage(matchedImageLoc)
+//    setDoctImage(matchedImageLoc)
      
-})
+// })
    
-       .catch((err) => {
-           console.error('Error fetching data:', err);
-       });
-}, []);
+//        .catch((err) => {
+//            console.error('Error fetching data:', err);
+//        });
+// }, []);
     return(
         <>
 
-        <div className="item" key={rowno}>
+        <div className="item" key={docID}>
     <div  className={`${isDefaultImage ? 'item-imgsBack' : 'item-img'}`} style={{backGroundColor:" #c6dffe"}}>
-{doctImage.length>0?
+
+{/* {doctImage.length>0?
   <img src={doctImage}   className={`${isDefaultImage ? 'item-imgs' : ''} `} alt="doc" />:
+  <i class="fas fa-user-md fa-10x"></i>
+  } */}
+
+{img?
+  <img src={`https://ik.imagekit.io/hg4fpytvry/product-images/tr:w-180,h-230,f-webp${img}`} className="item-imgs" alt="doc" />:
   <i class="fas fa-user-md fa-10x"></i>
   }
     
@@ -55,10 +64,10 @@ const [isDefaultImage, setIsDefaultImage] = useState(false);
     <div className="sider-contain">
        <div className="slider-heading">
           <h2>Dr. {firstName} {lastName}</h2>
-          <p>{primary_spl}</p>
-          <h5 className="text-center">{hospital_affliated} {state} {country_code}</h5>
+          <p>{primarySpl}</p>
+          <h5 className="text-center">{hospitalAffliated} {state} </h5>
        </div>
-       <Link to={ `/profile/${rowno}-${firstName}-${lastName}` } className="appointmentBtn allBtn" id="visitDoc">Visit Profile</Link>
+       <Link to={ `/doctor/${docID}-${firstName}-${lastName}` } className="appointmentBtn allBtn" id="visitDoc">Visit Profile</Link>
     </div>
  </div>
         </>
