eea2f2094ca5790be185a642b39415f6baae996b
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-04-01 15:26:21 +0530

    Updated the chat api in ChatList.js

--- src/components/LandingPage/ChatList.js
+++ src/components/LandingPage/ChatList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useRef } from 'react';
+import React, { useState, useEffect,useRef } from 'react'; 
 import axios from 'axios';
 import { backendHost } from '../../api-config'
 import Header from "../Header/Header";
