a424348e89355910c344c3b26e75de276633e069
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-04-12 15:53:25 +0530

    Update Home.js

--- src/components/LandingPage/Home.js
+++ src/components/LandingPage/Home.js
@@ -43,7 +43,7 @@ import FeaturedArticles from './FeaturedArticles';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import headers from '../../api-fetch';
 
-env.REACT_APP = 'http://117.241.171.115:8080/cures';
+env.REACT_APP = 'http://115.246.93.94:8080/cures';
 
 class Home extends Component {
    constructor(props){
