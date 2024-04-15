6ff2ed1fdf27c6f6539316b10ae2aa65d8f2a9a4
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-04-15 12:48:21 +0530

    Update ResponsePage.js

--- src/components/Profile/ResponsePage.js
+++ src/components/Profile/ResponsePage.js
@@ -17,15 +17,14 @@ const ResponsePage = () => {
     const[getResponse,setGetResponse]=useState('')
 
 
-   
     useEffect(()=>{
 
-      fetch(`${backendHost}/payment/get/payment-udpates/${responseObject.orderID}`)
-      .then(res=>res.json())
-      .then(json=>{
-        console.log("res",json)
-        setGetResponse(json)
-      })
+      // fetch(`${backendHost}/payment/get/payment-udpates/127a006e-1655-4630-a072-3aaba74d0ae0`)
+      // .then(res=>res.json())
+      // .then(json=>{
+      //   console.log("res",json)
+      //   setGetResponse(json)
+      // })
 
       function sendMessageToApp() {
         // const data =responseObject.orderID 
@@ -43,66 +42,79 @@ const ResponsePage = () => {
 
       sendMessageToApp()
 
+      const redirectURL = "https://www.all-cures.com/statusPayment" 
+     
+
+    // Redirecting to the URL
+    window.location.href = redirectURL;
+
+
+
+
     },[])
 
+
+    
   return (
-   <>
-<Header/>
-<div  style={{marginTop:"4rem"}}>
+//    <>
+// <Header/>
+// <div  style={{marginTop:"4rem"}}>
 
-  {!getResponse &&
-  (
-<>
+//   {!getResponse &&
+//   (
+// <>
 
-<div className="container d-flex justify-content-center mb-2" > 
-  <h4> Please wait while we complete your Payment...</h4>
-  </div>
+// <div className="container d-flex justify-content-center mb-2" > 
+//   <h4> Please wait while we complete your Payment...</h4>
+//   </div>
 
-  <div className="container d-flex justify-content-center mt-2 mb-5">
+//   <div className="container d-flex justify-content-center mt-2 mb-5">
  
-  <h2> Do not refresh the page...</h2>
-  </div>
+//   <h2> Do not refresh the page...</h2>
+//   </div>
 
-  </>
- ) }
-  {getResponse&&
-  <>
-  <div className="container d-flex justify-content-center">
+//   </>
+//  ) }
+//   {getResponse&&
+//   <>
+//   <div className="container d-flex justify-content-center">
 
-  <div className="card shadow-lg pt-3 m-3" style={{minHeight:"400px",width:"500px"}}>
-    <div className="card-body">
-      <div className="d-flex justify-content-center">
-      <Avatar sx={{ bgcolor: green[800], width: 80, height: 80 }}>
-      < CheckIcon  sx={{  width: 56, height: 56 }} />
-                            </Avatar>
+//   <div className="card shadow-lg pt-3 m-3" style={{minHeight:"400px",width:"500px"}}>
+//     <div className="card-body">
+//       <div className="d-flex justify-content-center">
+//       <Avatar sx={{ bgcolor: green[800], width: 80, height: 80 }}>
+//       < CheckIcon  sx={{  width: 56, height: 56 }} />
+//                             </Avatar>
                
-      </div>
+//       </div>
 
-      <div className="d-flex justify-content-center" style={{color:"green"}}>
-                 <h2>Success</h2>
-                </div>
+//       <div className="d-flex justify-content-center" style={{color:"green"}}>
+//                  <h2>Success</h2>
+//                 </div>
 
-                <div className="mt-4 text-center">
-        <p style={{ fontSize: "20px" }}>
-          Your transaction has been completed successfully.
-          Please check your email for appointment details.
-        </p>
-      </div>
-    </div>
-  </div>
-  </div>
+//                 <div className="mt-4 text-center">
+//         <p style={{ fontSize: "20px" }}>
+//           Your transaction has been completed successfully.
+//           Please check your email for appointment details.
+//         </p>
+//       </div>
+//     </div>
+//   </div>
+//   </div>
  
   
-  </>
+//   </>
   
-  // <h3>Payment Status:{getResponse}</h3>
-  }
+//   // <h3>Payment Status:{getResponse}</h3>
+//   }
 
-</div>
+// </div>
 
-<Footer/>
+// <Footer/>
 
-    </>
+//     </>
+
+<></>
   )
 }
 
