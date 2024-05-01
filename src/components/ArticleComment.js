71987d2a57981b2f7b9dd3c73c22b325067d6313
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-05-01 15:00:22 +0530

    Update ArticleComment.js

--- src/components/ArticleComment.js
+++ src/components/ArticleComment.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import axios from 'axios';
 import { backendHost } from '../api-config';
 import { Button, Modal } from "react-bootstrap";
