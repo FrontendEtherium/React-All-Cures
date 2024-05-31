4868fe44814214543f750b75efa438f102c608d0
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-05-31 11:35:41 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -215,7 +215,7 @@ import { userAccess } from "../UserAccess";
                                <div class="collapse navbar-collapse" id="navbarNavDropdown">
   <ul class="navbar-nav">
   <li class="nav-item">
-      <Link class="nav-link" to="/">Home</Link>
+      <Link class="nav-link" to="/cures/home">Home</Link>
     </li>
     <li class="nav-item dropdown">
       <Link class="nav-link dropdown-toggle" to="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</Link>
