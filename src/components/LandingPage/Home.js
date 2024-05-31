63582a28e10ace2296caed2bed0a3203fa662360
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-05-31 11:34:33 +0530

    Update Home.js

--- src/components/LandingPage/Home.js
+++ src/components/LandingPage/Home.js
@@ -433,7 +433,7 @@ class Home extends Component {
                                   <div class="collapse navbar-collapse" id="navbarNavDropdown">
      <ul class="navbar-nav">
         <li class="nav-item">
-         <Link className="nav-link" to="/">Home</Link>
+         <Link className="nav-link" to="/cures/home">Home</Link>
        </li>
        <li class="nav-item dropdown">
          <Link class="nav-link dropdown-toggle" to="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</Link>
