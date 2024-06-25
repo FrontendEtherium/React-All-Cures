2f5aada77b27e1b019e864ff2f8fe908eeb4bb55
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2024-06-25 15:38:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Main from './components/MainComponent';
 import CookieConsent from "react-cookie-consent";
 import AppBanner from "./components/LandingPage/AppBanner";
-
+import { Link } from 'react-router-dom';
   
 const App = () => {
     
@@ -27,8 +27,7 @@ const App = () => {
       <Main/>
       <CookieConsent style={{ background: "#022a3c"}}>
         <div className="container m-3">
-      We use cookies to ensure you have the best browsing experience on our website. By using our site, you acknowledge that you have read and understood our <a className="text-underline text-white" href="/">Cookie Policy</a> & <a className="text-underline text-white" href="https://etheriumtech.com/privacy.html" target="_blank" rel="noreferrer">Privacy Policy</a>.
-      </div>
+        We use cookies to ensure you have the best browsing experience on our website. By using our site, you acknowledge that you have read and understood our <Link className="text-underline text-white" to="/">Cookie Policy</Link> & <Link className="text-underline text-white" to="/privacy" target="_blank" rel="noreferrer">Privacy Policy</Link> </div>
       </CookieConsent>
   <AppBanner/>
     </div>
