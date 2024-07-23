fe81789cf80538eed4ed2b9a3dc71ab2ca21e1c6
mansisharma18 <127491930+mansisharma18@users.noreply.github.com>
2024-07-23 13:06:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Main from './components/MainComponent';
 import CookieConsent from "react-cookie-consent";
 import AppBanner from "./components/LandingPage/AppBanner";
-import { Link } from 'react-router-dom';
 
+  
 const App = () => {
+    
+    // useEffect(() => {
+    //   const canonicalLink = document.createElement('link');
+    //   canonicalLink.rel = 'canonical';
+    //   canonicalLink.href = window.location.href;
+    //   document.head.appendChild(canonicalLink);
+
+    //   console.log('Canonical link:', canonicalLink);
+
+  
+    //   return () => {
+    //     document.head.removeChild(canonicalLink);
+    //   };
+
+
+    // }, []);
+  
   return (
     <div>
-      <Main />
-      <CookieConsent
-        style={{
-          background: "#022a3c",
-          width: "50%",
-          margin: "0 auto",
-          borderRadius: "10px",
-          padding: "10px 20px",
-          position: "fixed",
-          bottom: "20px",
-          left: "51%",
-          transform: "translateX(-50%)",
-          zIndex: "1000",
-          textAlign: "center"
-        }}
-        buttonStyle={{
-          background: "#4585FF",
-          color: "white",
-          fontSize: "14px",
-          borderRadius: "5px",
-          padding: "5px 10px",
-          marginLeft: "15px",
-          border: "none"
-        }}
-      >
-        <div style={{ color: "#fff", margin: "0" }}>
-          We use cookies to ensure you have the best browsing experience on our website. By using our site, you acknowledge that you have read and understood our <Link className="text-underline text-white" to="/">Cookie Policy</Link> & <Link className="text-underline text-white" to="/privacy" target="_blank" rel="noreferrer">Privacy Policy</Link>.
-        </div>
+      <Main/>
+      <CookieConsent style={{ background: "#022a3c"}}>
+        <div className="container m-3">
+      We use cookies to ensure you have the best browsing experience on our website. By using our site, you acknowledge that you have read and understood our <a className="text-underline text-white" href="/">Cookie Policy</a> & <a className="text-underline text-white" href="https://etheriumtech.com/privacy.html" target="_blank" rel="noreferrer">Privacy Policy</a>.
+      </div>
       </CookieConsent>
-      <AppBanner />
+     <AppBanner/>
     </div>
   );
 }
