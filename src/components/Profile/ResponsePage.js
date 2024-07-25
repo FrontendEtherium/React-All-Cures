64fad3765dc84fce3ef93439dbd0dd96e0811535
mansisharma18 <127491930+mansisharma18@users.noreply.github.com>
2024-07-25 13:09:04 +0530

    Update ResponsePage.js

--- src/components/Profile/ResponsePage.js
+++ src/components/Profile/ResponsePage.js
@@ -9,15 +9,15 @@ import CheckIcon from '@mui/icons-material/Check';
 
 const ResponsePage = () => {
 
-    // const response=localStorage.getItem('apiResponse')
-    // // console.log('responsepage',response)
-    // const responseObject = JSON.parse(response);
-    // // console.log('res',responseObject.orderID)
+    const response=localStorage.getItem('apiResponse')
+    // console.log('responsepage',response)
+    const responseObject = JSON.parse(response);
+    // console.log('res',responseObject.orderID)
 
-    // const[getResponse,setGetResponse]=useState('')
+    const[getResponse,setGetResponse]=useState('')
 
 
-    // useEffect(()=>{
+    useEffect(()=>{
 
 
     //   function sendMessageToApp() {
@@ -41,27 +41,24 @@ const ResponsePage = () => {
 
 
 
-    // },[])
+
+    async function sendMessageAndRedirect() {
+      try {
+        // Send a message to the React Native app
+        await window.ReactNativeWebView.postMessage("Payment Successful");
+    
+        // Redirecting to the URL
+        const redirectURL = "https://www.all-cures.com/statusPayment";
+        window.location.href = redirectURL;
+      } catch (error) {
+        console.error("Error occurred:", error);
+      }
+    }
+
+    sendMessageAndRedirect();
 
 
-    const response=localStorage.getItem('apiResponse')
-    console.log('responsepage',response)
-    const responseObject = JSON.parse(response);
-    console.log('res',responseObject.orderID)
-
-    const[getResponse,setGetResponse]=useState('')
-    useEffect(()=>{
-
-        fetch(`${backendHost}/payment/get/payment-udpates/${responseObject.orderID}`)
-        .then(res=>res.json())
-        .then(json=>{
-          console.log("res",json)
-          setGetResponse(json)
-        })
-  
-       
-  
-      },[])
+    },[])
 
 
     
@@ -69,60 +66,15 @@ const ResponsePage = () => {
 
 <>
 
-<div  style={{marginTop:"4rem"}}>
 
-  {!getResponse &&
-  (
-<>
-
-<div className="container d-flex justify-content-center mb-2" > 
-  <h4> Please wait while we complete your Payment...</h4>
-  </div>
-
-  <div className="container d-flex justify-content-center mt-2 mb-5">
- 
-  <h2> Do not refresh the page...</h2>
-  </div>
-
-  </>
- ) }
-  {getResponse&&
-  <>
-  <div className="container d-flex justify-content-center">
-
-  <div className="card shadow-lg pt-3 m-3" style={{minHeight:"400px",width:"500px"}}>
-    <div className="card-body">
-      <div className="d-flex justify-content-center">
-      <Avatar sx={{ bgcolor: green[800], width: 80, height: 80 }}>
-      < CheckIcon  sx={{  width: 56, height: 56 }} />
-                            </Avatar>
-               
-      </div>
-
-      <div className="d-flex justify-content-center" style={{color:"green"}}>
-                 <h2>Success</h2>
-                </div>
-
-                <div className="mt-4 text-center">
-        <p style={{ fontSize: "20px" }}>
-          Your transaction has been completed successfully.
-          Please check your email for appointment details.
-        </p>
-      </div>
-    </div>
-  </div>
-  </div>
- 
-  
-  </>
-  
-  // <h3>Payment Status:{getResponse}</h3>
-  }
-
-</div>
 
 </>
   )
 }
 
 export default ResponsePage
+
+
+
+
+
