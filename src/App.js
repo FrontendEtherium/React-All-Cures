5eeef0912d247ab91f86294fd572e2169f8b0975
mansisharma18 <127491930+mansisharma18@users.noreply.github.com>
2024-07-26 16:26:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,75 @@
-import React, { useEffect, useState } from "react";
-import Main from './components/MainComponent';
+import React from "react";
+import Main from "./components/MainComponent";
 import CookieConsent from "react-cookie-consent";
 import AppBanner from "./components/LandingPage/AppBanner";
+import { Link } from "react-router-dom";
+import "./cookie.css"
 
-  
 const App = () => {
-    
-    // useEffect(() => {
-    //   const canonicalLink = document.createElement('link');
-    //   canonicalLink.rel = 'canonical';
-    //   canonicalLink.href = window.location.href;
-    //   document.head.appendChild(canonicalLink);
-
-    //   console.log('Canonical link:', canonicalLink);
-
-  
-    //   return () => {
-    //     document.head.removeChild(canonicalLink);
-    //   };
-
-
-    // }, []);
-  
   return (
     <div>
-      <Main/>
-      <CookieConsent style={{ background: "#022a3c"}}>
-        <div className="container m-3">
-      We use cookies to ensure you have the best browsing experience on our website. By using our site, you acknowledge that you have read and understood our <a className="text-underline text-white" href="/">Cookie Policy</a> & <a className="text-underline text-white" href="https://etheriumtech.com/privacy.html" target="_blank" rel="noreferrer">Privacy Policy</a>.
+      <Main />
+      <div>
+        <CookieConsent
+          style={{
+            background: "#022a3c",
+            width: "80%",
+
+            borderRadius: "10px",
+
+            position: "fixed",
+            bottom: "20px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            zIndex: "1000",
+            textAlign: "center",
+            maxWidth: "80%", // For better responsiveness
+       
+          }}
+          buttonStyle={{
+            background: "#4585FF",
+            color: "white",
+
+            fontSize: "12px",
+            borderRadius: "5px",
+
+            border: "none",
+            alignItem: "center",
+        
+          }}
+        >
+          <div
+            style={{
+              color: "#fff",
+              margin: "0",
+              maxWidth: "100%",
+              fontSize: "15px",
+            
+            }}
+          >
+            We use cookies to ensure you have the best browsing experience on
+            our website. By using our site, you acknowledge that you have read
+            and understood our{" "}
+            <a className="text-underline text-white" href="/">
+              Cookie Policy
+            </a>{" "}
+            &{" "}
+            <a
+              className="text-underline text-white"
+              href="/privacy"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Privacy Policy
+            </a>
+            .
+          </div>
+        </CookieConsent>
       </div>
-      </CookieConsent>
-     <AppBanner/>
+
+      <AppBanner />
     </div>
   );
-}
+};
 
 export default App;
