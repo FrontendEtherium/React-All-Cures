9b2e8d8a3615a5aab0c1c7fdd378525e84db7bbb
mansisharma18 <127491930+mansisharma18@users.noreply.github.com>
2024-08-08 12:43:29 +0530

    Add files via upload

---
+++ src/components/Profile/PaymentRedirect.js
@@ -0,0 +1,59 @@
+import React,{useEffect} from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+const PaymentRedirect = () => {
+
+    useEffect(()=>{
+
+    // const enc=localStorage.getItem('encKey')
+    // console.log("enc",enc)
+      
+
+    const params = new URLSearchParams(window.location.search);
+    const enc = params.get('encRequest');
+    const accessCode = params.get('accessCode');
+
+    // console.log(enc)
+    // console.log(accessCode)
+        
+      const form = document.createElement('form');
+      form.setAttribute('method', 'post');
+      form.setAttribute('action', 'https://secure.ccavenue.com/transaction.do?command=initiateTransaction');
+      form.style.display = 'none'; // Hide the form
+    
+      // Create and append hidden input fields for encRequest and accessCode
+      const encRequestInput = document.createElement('input');
+      encRequestInput.setAttribute('type', 'hidden');
+      encRequestInput.setAttribute('name', 'encRequest');
+      encRequestInput.setAttribute('value',enc);
+      
+      const accessCodeInput = document.createElement('input');
+      accessCodeInput.setAttribute('type', 'hidden');
+      accessCodeInput.setAttribute('name', 'access_code');
+    //   accessCodeInput.setAttribute('value', 'AVNH05LB56CF25HNFC');
+      accessCodeInput.setAttribute('value',  accessCode);
+    
+      // Append input fields to the form
+      form.appendChild(encRequestInput);
+      form.appendChild(accessCodeInput);
+    
+      // Append the form to the document body
+      document.body.appendChild(form);
+    
+      // Submit the form
+      form.submit();
+
+
+
+    },[])
+  return (
+    <div>
+        <Header/>
+        <div style={{height:"80vh"}}></div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default PaymentRedirect
---
+++ src/components/Profile/ResponsePage.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import {useState,useEffect} from 'react'
+import { backendHost } from "../../api-config";
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import Avatar from "@mui/material/Avatar";
+import { green } from '@mui/material/colors';
+import CheckIcon from '@mui/icons-material/Check';
+
+const ResponsePage = () => {
+
+    const response=localStorage.getItem('apiResponse')
+    // console.log('responsepage',response)
+    const responseObject = JSON.parse(response);
+    // console.log('res',responseObject.orderID)
+
+    const[getResponse,setGetResponse]=useState('')
+
+
+    useEffect(()=>{
+
+
+    //   function sendMessageToApp() {
+        
+    //     // Send a message to the React Native app
+    //     window.ReactNativeWebView.postMessage("Payment Successful");
+
+
+    //   }
+
+     
+
+    //   sendMessageToApp()
+
+    //   const redirectURL = "https://www.all-cures.com/statusPayment" 
+     
+
+    // // Redirecting to the URL
+    // window.location.href = redirectURL;
+
+
+
+
+
+    async function sendMessageAndRedirect() {
+      try {
+        // Send a message to the React Native app
+        await window.ReactNativeWebView.postMessage("Payment Successful");
+    
+        // Redirecting to the URL
+        const redirectURL = "https://www.all-cures.com/statusPayment";
+        window.location.href = redirectURL;
+      } catch (error) {
+        console.error("Error occurred:", error);
+      }
+    }
+
+    sendMessageAndRedirect();
+
+
+    },[])
+
+
+    
+  return (
+
+<>
+
+
+
+</>
+  )
+}
+
+export default ResponsePage
+
+
+
+
+
