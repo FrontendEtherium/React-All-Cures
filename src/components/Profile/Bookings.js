d3dd46b2e4cf11480b183c08e5c8e363461a1946
mansisharma18 <127491930+mansisharma18@users.noreply.github.com>
2024-08-09 13:19:50 +0530

    Update Bookings.js

--- src/components/Profile/Bookings.js
+++ src/components/Profile/Bookings.js
@@ -8,49 +8,72 @@ import { useHistory } from "react-router-dom";
 import { userId } from "../UserId";
 import { userAccess } from "../UserAccess";
 
-
 import { Container, Button } from "react-bootstrap";
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-
-
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const Bookings = () => {
+  const history = useHistory();
+  const [value, setValue] = React.useState(0);
+  const [data, setData] = useState([]);
+  const [docData, setDocData] = useState([]);
 
-    const history = useHistory();
-    const [value, setValue] = React.useState(0);
-    const[data,setData]=useState([])
+  const docID= localStorage.getItem('doctorid')
+  // const docID = 14485
 
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(`${backendHost}/appointments/get/user/87`); //change 87 to registration ID
-          // if (!response.ok) {
-          //   return;
-          // }
-          const json = await response.json();
-          setData(json);
-          console.log(json);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${backendHost}/appointments/get/user/${userId}`); //change 87 to registration ID
+        // if (!response.ok) {
+        //   return;
+        // }
+        const json = await response.json();
+        setData(json);
+        console.log(json);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    const fetchDocData = async () => {
+      try {
+        const response = await fetch(`${backendHost}/appointments/get/${docID}`); 
+        // if (!response.ok) {
+        //   return;
+        // }
+        const json = await response.json();
+        setDocData(json);
+        console.log(json);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    if(docID){
+      fetchDocData()
+      console.log("doc details",docID)
+    }
+
+    else{
+      fetchData();
+      console.log("user details")
+    }
+
     
-      fetchData(); 
     
-    }, []); 
-    
+  }, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
   function CustomTabPanel(props) {
     const { children, value, index, ...other } = props;
-  
+
     return (
       <div
         role="tabpanel"
@@ -67,91 +90,218 @@ const Bookings = () => {
       </div>
     );
   }
-  
+
   CustomTabPanel.propTypes = {
     children: PropTypes.node,
     index: PropTypes.number.isRequired,
     value: PropTypes.number.isRequired,
   };
-  
+
   function a11yProps(index) {
     return {
       id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
     };
   }
- 
 
   return (
     <div>
-          <Header history={history} />
-          <div className="d-flex justify-content-center mt-5">
+      <Header history={history} />
+      <div className="d-flex justify-content-center mt-5">
+        <Box sx={{ width: { xs: "100%", md: "50%" } }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              centered
+              variant="scrollable"
+              scrollButtons
+              allowScrollButtonsMobile
+              aria-label="scrollable force tabs example"
+            >
+              <Tab label="Upcoming Meetings" {...a11yProps(0)} />
+              <Tab label="Completed Meetings" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+          {data &&
+              data
+                .filter((d) => d.status == 0)
+                .map((filteredData) => (
+                  <div key={filteredData.id}>
+                    <div className="card shadow-sm mt-2 mb-4">
+                      <div className="card-body">
+                        <div className="d-flex align-items-center">
+                          <div className="mr-5">
+                            <div className="mb-2 bookings">
+                              {" "}
+                              <p>
+                                {" "}
+                                <span className="fw-bold">
+                                  Dr. {filteredData.doctorName}{" "}
+                                </span>
+                              </p>{" "}
+                            </div>
+                            <div className="mb-2 bookings">
+                              <p>
+                                {" "}
+                                Booking Date: {filteredData.appointmentDate}
+                              </p>{" "}
+                            </div>
+                            <div className="mb-2 bookings">
+                              {" "}
+                              <p>
+                                {" "}
+                                Booking Time: {filteredData.startTime} -{" "}
+                                {filteredData.endTime}
+                              </p>{" "}
+                            </div>
+                          </div>
+                          <div className="bookings-img text-center ml-auto">
+                            <i className="fas fa-user-md fa-6x"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
 
-          <Box sx={{ width: { xs: '100%', md: '50%' } }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange}  centered variant="scrollable"
-  scrollButtons
-  allowScrollButtonsMobile
-  aria-label="scrollable force tabs example">
-          <Tab label="Upcoming Meetings" {...a11yProps(0)} />
-          <Tab label="Completed Meetings" {...a11yProps(1)} />
-        
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={value} index={0}>
-        No Upcoming Meetings
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-      {data && data.filter(d => d.status === 2).map(filteredData => (
-    <div key={filteredData.id}>
-      <div className="card shadow-sm mt-2 mb-4">
-  
-  <div className="card-body">
-    <div className="d-flex align-items-center">
-    <div className="mr-5"> 
-   <div className="mb-2 bookings"> <p> <span  className="fw-bold">Dr. {filteredData.doctorName} </span></p> </div>
-   <div className="mb-2 bookings"><p>  Booking Date: {filteredData.appointmentDate}</p> </div>
-   <div className="mb-2 bookings"> <p>   Booking Time: {filteredData.startTime} - {filteredData.endTime}</p> </div>
+{docData &&
+              docData
+                .filter((d) => d.status == 0)
+                .map((filteredData) => (
+                  <div key={filteredData.id}>
+                    <div className="card shadow-sm mt-2 mb-4">
+                      <div className="card-body">
+                        <div className="d-flex align-items-center">
+                          <div className="mr-5">
+                            <div className="mb-2 bookings">
+                              {" "}
+                              <p>
+                                {" "}
+                                <span className="fw-bold">
+                                 {filteredData.userName}{" "}
+                                </span>
+                              </p>{" "}
+                            </div>
+                            <div className="mb-2 bookings">
+                              <p>
+                                {" "}
+                                Booking Date: {filteredData.appointmentDate}
+                              </p>{" "}
+                            </div>
+                            <div className="mb-2 bookings">
+                              {" "}
+                              <p>
+                                {" "}
+                                Booking Time: {filteredData.startTime} -{" "}
+                                {filteredData.endTime}
+                              </p>{" "}
+                            </div>
+                          </div>
+                          <div className="bookings-img text-center ml-auto">
+                            <i className="fas fa-user-md fa-6x"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            {data &&
+              data
+                .filter((d) => d.status == 2)
+                .map((filteredData) => (
+                  <div key={filteredData.id}>
+                    <div className="card shadow-sm mt-2 mb-4">
+                      <div className="card-body">
+                        <div className="d-flex align-items-center">
+                          <div className="mr-5">
+                            <div className="mb-2 bookings">
+                              {" "}
+                              <p>
+                                {" "}
+                                <span className="fw-bold">
+                                  Dr. {filteredData.doctorName}{" "}
+                                </span>
+                              </p>{" "}
+                            </div>
+                            <div className="mb-2 bookings">
+                              <p>
+                                {" "}
+                                Booking Date: {filteredData.appointmentDate}
+                              </p>{" "}
+                            </div>
+                            <div className="mb-2 bookings">
+                              {" "}
+                              <p>
+                                {" "}
+                                Booking Time: {filteredData.startTime} -{" "}
+                                {filteredData.endTime}
+                              </p>{" "}
+                            </div>
+                          </div>
+                          <div className="bookings-img text-center ml-auto">
+                            <i className="fas fa-user-md fa-6x"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+
+{docData &&
+              docData
+                .filter((d) => d.status == 2)
+                .map((filteredData) => (
+                  <div key={filteredData.id}>
+                    <div className="card shadow-sm mt-2 mb-4">
+                      <div className="card-body">
+                        <div className="d-flex align-items-center">
+                          <div className="mr-5">
+                            <div className="mb-2 bookings">
+                              {" "}
+                              <p>
+                                {" "}
+                                <span className="fw-bold">
+                                 {filteredData.userName}{" "}
+                                </span>
+                              </p>{" "}
+                            </div>
+                            <div className="mb-2 bookings">
+                              <p>
+                                {" "}
+                                Booking Date: {filteredData.appointmentDate}
+                              </p>{" "}
+                            </div>
+                            <div className="mb-2 bookings">
+                              {" "}
+                              <p>
+                                {" "}
+                                Booking Time: {filteredData.startTime} -{" "}
+                                {filteredData.endTime}
+                              </p>{" "}
+                            </div>
+                          </div>
+                          <div className="bookings-img text-center ml-auto">
+                            <i className="fas fa-user-md fa-6x"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+          </CustomTabPanel>
+        </Box>
+      </div>
+
+      <Footer />
     </div>
-    <div  className="bookings-img text-center ml-auto">
-    <i className="fas fa-user-md fa-6x"></i>
-    </div>
+  );
+};
 
-    </div>
-  
- 
-  </div>
-</div>
-       
-    </div>
-))}
-      </CustomTabPanel>
-      
-    </Box>
-          </div>
-
-          <Footer/>
-
-
-    </div>
-  )
-}
-
-export default Bookings
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default Bookings;
