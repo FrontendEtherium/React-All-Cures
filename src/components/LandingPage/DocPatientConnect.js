ed16e552cd869d5e9f83e194095978f44c53e7d3
mansisharma18 <127491930+mansisharma18@users.noreply.github.com>
2024-08-09 16:07:18 +0530

    Update DocPatientConnect.js

--- src/components/LandingPage/DocPatientConnect.js
+++ src/components/LandingPage/DocPatientConnect.js
@@ -31,7 +31,6 @@ import { PickersDay } from "@mui/x-date-pickers/PickersDay";
 import { styled } from "@mui/material/styles";
 import moment from "moment";
 import { Modal, Alert } from "react-bootstrap";
-import DocModal from "./DocModal";
 import Heart from "../../assets/img/heart.png";
 import Test from "./test";
 
