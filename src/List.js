439b3825d0c6db7e7b43d4817981b906594518a0
FrontendEtherium <developeretherium@gmail.com>
2025-02-03 13:45:14 +0530

    Update List.js

--- src/List.js
+++ src/List.js
@@ -19,6 +19,7 @@ export default class App extends Component {
             currentPage: 0
             
         };
+        
         this.handlePageClick = this
             .handlePageClick
             .bind(this);
