d325ba92b41b7f3a2ce6400eed642a58a71d689e
Aashu-crypto <ashugandotra14@gmail.com>
2025-09-25 10:55:24 +0530

    new

--- src/components/Disease/Disease.js
+++ src/components/Disease/Disease.js
@@ -338,7 +338,7 @@ const Disease = () => {
               name={state.items.dc_name}
             />
           </div>
-          {state.ads &&
+          {/* {state.ads &&
             (state.ads !== "https://all-cures.com:444All Ads are Served" ? (
               <div className="d-flex justify-content-center">
                 <img
@@ -364,7 +364,7 @@ const Disease = () => {
                   alt="adhhh"
                 />
               </button>
-            ))}
+            ))} */}
         </div>
         <Col md={7} id="page-content-wrapper" className="col-xs-12 pb-5">
           <div id="center-well" ref={containerRef}>
